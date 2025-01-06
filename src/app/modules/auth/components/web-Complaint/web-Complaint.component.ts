89e4621a2245e235b391f2a207af7058a272a71d
Swapnil P <swapnil@Swapnils-MacBook-Air.local>
2025-01-06 16:08:47 +0530

    swapnil

--- src/app/modules/auth/components/web-Complaint/web-Complaint.component.ts
+++ src/app/modules/auth/components/web-Complaint/web-Complaint.component.ts
@@ -30,6 +30,7 @@ export class WebComplaintsComponent
   data: any;
   withoutlogin: { formName: string; title: string; searchButton: string; searchbutton_name: string; addButton: string; addButton_name: string; scanButton: string; scanButton_name: string; };
   firstWord: any;
+  loginDetail_: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -67,55 +68,69 @@ this.withoutlogin =
 }
 
 
-this.loginDetail = {
-  "clientID": this.firstWord,
-  "allowNewCompanyID": false,
-  "device_type_permission": [],
-  "default_type": "Device",
-  "start_node": "Default",
-  "description": "Developer",
-  "health_check": false,
-  "userID": "swapnil",
-  "telegramID": 123456789,
-  "cognito_update": false,
-  "device_timeout": false,
-  "enable_user": false,
-  "companyID": "tree_creation",
-  "password": "123456789",
-  "escalation_sms": true,
-  "report_to": null,
-  "alert_telegram": true,
-  "escalation_email": false,
-  "default_module": "Dashboard - Group",
-  "location_object": "",
-  "redirectionURL": "/dashboard",
-  "email": "swapnil@wimate.in",
-  "key": "U2FsdGVkX18CUyAdixEzQANiBbYUfka2I/kl4mN0BFQ=",
-  "device_permission": [],
-  "alert_email": false,
-  "mobile": "8174941153",
-  "alert_sms": false,
-  "permission_ID": "Dreamboard Module",
-  "default_dev": null,
-  "token": "d1RTlcIYRIm604ZhkZgaB9:APA91bGpU3KCIorhkY_3nwNrhlG2rdBwJNLbHKzdsezLbMZSFBb5keUKYI59G19FueJJjwnjDW_nSf-wtz1v2MF-18NV8WbywLrb8rdyBgnrTk6AKgoay7M",
-  "mobile_privacy": "Invisible",
-  "allowOtherClient": false,
-  "escalation_telegram": false,
-  "alert_timeout": false,
-  "allowOtherCompanyID": null,
-  "alert_levels": "NONE",
-  "default_loc": null,
-  "location_permission": [
-      "All"
-  ],
-  "form_permission": [
-      "All"
-  ],
-  "updated": "2024-12-27T07:56:24.527Z",
-  "allowNewClient": {},
-  "disable_user": false,
-  "username": `${this.firstWord}_withoutLogin`
-}
+// this.loginDetail = {
+//   "clientID": this.firstWord,
+//   "allowNewCompanyID": false,
+//   "device_type_permission": [],
+//   "default_type": "Device",
+//   "start_node": "Default",
+//   "description": "Developer",
+//   "health_check": false,
+//   "userID": "swapnil",
+//   "telegramID": 123456789,
+//   "cognito_update": false,
+//   "device_timeout": false,
+//   "enable_user": false,
+//   "companyID": "tree_creation",
+//   "password": "123456789",
+//   "escalation_sms": true,
+//   "report_to": null,
+//   "alert_telegram": true,
+//   "escalation_email": false,
+//   "default_module": "Dashboard - Group",
+//   "location_object": "",
+//   "redirectionURL": "/dashboard",
+//   "email": "swapnil@wimate.in",
+//   "key": "U2FsdGVkX18CUyAdixEzQANiBbYUfka2I/kl4mN0BFQ=",
+//   "device_permission": [],
+//   "alert_email": false,
+//   "mobile": "8174941153",
+//   "alert_sms": false,
+//   "permission_ID": "Dreamboard Module",
+//   "default_dev": null,
+//   "token": "d1RTlcIYRIm604ZhkZgaB9:APA91bGpU3KCIorhkY_3nwNrhlG2rdBwJNLbHKzdsezLbMZSFBb5keUKYI59G19FueJJjwnjDW_nSf-wtz1v2MF-18NV8WbywLrb8rdyBgnrTk6AKgoay7M",
+//   "mobile_privacy": "Invisible",
+//   "allowOtherClient": false,
+//   "escalation_telegram": false,
+//   "alert_timeout": false,
+//   "allowOtherCompanyID": null,
+//   "alert_levels": "NONE",
+//   "default_loc": null,
+//   "location_permission": [
+//       "All"
+//   ],
+//   "form_permission": [
+//       "All"
+//   ],
+//   "updated": "2024-12-27T07:56:24.527Z",
+//   "allowNewClient": {},
+//   "disable_user": false,
+//   "username": `${this.firstWord}_withoutLogin`
+// }
+
+
+const test  = await this.api.GetMaster(this.firstWord.toLowerCase()+'_'+'withoutlogin#user#main',1)
+
+console.log("USER RESPONSE:",test)
+
+this.loginDetail_ =  JSON.parse(JSON.parse(JSON.stringify(test.metadata)))
+
+console.log("LOGIN DETAIL:",this.loginDetail_)
+
+delete this.loginDetail_.profile_picture;
+
+
+console.log("AFTER REMOVE OF pic:",this.loginDetail_)
 
 
     
@@ -153,7 +168,7 @@ this.loginDetail = {
         // console.log(this.url_result,)
         // const timestamp = new Date().getTime();
         // this.url=`https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/WIMATE_ADMIN/Without+Login/src/index.html?loginDetail="{\"username\":\"Wimate_Test\",\"clientID\":\"Climaveneta\",\"username_\":\"12345678\",\"name\":\"Wimate_Admin\",\"user_type\":\"Admin\",\"password\":\"DUMMY\",\"role\":\"Admin\",\"token\":\"admin-token\",\"permission_ID\":\"Dreamboard Module\"}"&withoutlogin="{\"formName\":\"Testing Web Complaint\",\"title\":\"How Can we help you\",\"searchButton\":\"yes\",\"searchbutton_name\":\"Search\",\"addButton\":\"yes\",\"addButton_name\":\"Create Ticket\",\"scanButton\":\"yes\",\"scanButton_name\":\"Scan QR Code\"}"`
-        this.url = `https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/WIMATE_ADMIN/Without%20Login/src/index.html?`+`&loginDetail=${JSON.stringify(JSON.stringify(this.loginDetail))}`+`&withoutlogin=${JSON.stringify(JSON.stringify(this.withoutlogin))}`
+        this.url = `https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/WIMATE_ADMIN/Without%20Login/src/index.html?`+`&loginDetail=${JSON.stringify(JSON.stringify(this.loginDetail_))}`+`&withoutlogin=${JSON.stringify(JSON.stringify(this.withoutlogin))}`
         // this.url =`https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/WIMATE_ADMIN/Without Login/src/index.html?loginDetail="{\"username\":\"Wimate_Test\",\"clientID\":\"Climaveneta\",\"username_\":\"12345678\",\"name\":\"Wimate_Admin\",\"user_type\":\"Admin\",\"password\":\"DUMMY\",\"role\":\"Admin\",\"token\":\"admin-token\",\"permission_ID\":\"Dreamboard Module\"}"&withoutlogin="{\"formName\":\"Testing Web Complaint\",\"title\":\"How Can we help you\",\"searchButton\":\"yes\",\"searchbutton_name\":\"Search\",\"addButton\":\"yes\",\"addButton_name\":\"Create Ticket\",\"scanButton\":\"yes\",\"scanButton_name\":\"Scan QR Code\"}"`
         this.send_data = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
         console.log(this.send_data)
