c940044068e48447cbd07ba97b1182fe31ba8ad4
Swapnil P <swapnil@Swapnils-MacBook-Air.local>
2025-01-09 18:53:54 +0530

    swapnil

--- src/app/pages/dream-id/dream-id.component.ts
+++ src/app/pages/dream-id/dream-id.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef,Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { APIService } from 'src/app/API.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Route } from "@angular/router";
@@ -11,7 +11,7 @@ import { param } from 'jquery';
   templateUrl: './dream-id.component.html',
   styleUrl: './dream-id.component.scss'
 })
-export class DreamIdComponent implements OnInit{
+export class DreamIdComponent implements OnInit {
   url_result: any;
   url: string;
   send_data: any;
@@ -29,7 +29,9 @@ export class DreamIdComponent implements OnInit{
   formId: string | null;
   recordId: string | null;
   project: any;
-  
+  iframeSrc: string;
+  // cacheBustingToken = new Date().getTime();
+
   constructor(
 
     private apiService: APIService,
@@ -37,234 +39,248 @@ export class DreamIdComponent implements OnInit{
     private sanitizer: DomSanitizer,
     private route: ActivatedRoute,
 
-  ){}
+  ) { }
 
   async ngOnInit() {
 
+
+    const cacheBustingToken = new Date().getTime();
 
     console.log("DREAM BOARD ID IFRAME")
 
     this.login_detail = localStorage.getItem('userAttributes')
 
     this.login_string = JSON.stringify(localStorage.getItem('userAttributes'))
-    console.log("LOGIN DETAIL FROM LOCAL STORAGE:",this.login_detail)
+    console.log("LOGIN DETAIL FROM LOCAL STORAGE:", this.login_detail)
 
 
     this.theme = localStorage.getItem('kt_theme_mode_menu')    //theme code light or dark
 
-     this.loginDetail_string = JSON.parse(this.login_detail) 
-    console.log("AFTER JSON STRINGIFY",this.loginDetail_string)
+    this.loginDetail_string = JSON.parse(this.login_detail)
+    console.log("AFTER JSON STRINGIFY", this.loginDetail_string)
 
     delete this.loginDetail_string.profile_picture;
 
 
-    console.log("AFTER REMOVE OF pic:",this.loginDetail_string)
-    
-    // this.loginDetail = JSON.parse(localStorage.getItem("currentUser")) 
-      this.client=this.loginDetail_string.clientID
-      this.user=this.loginDetail_string.username
+    console.log("AFTER REMOVE OF pic:", this.loginDetail_string)
 
-    console.log("CLIENT ID",this.client)
-    console.log("USER ID",this.user)
-    
-    const test = await this.apiService.GetMaster(this.user+'#user#main',1);
+    // this.loginDetail = JSON.parse(localStorage.getItem("currentUser")) 
+    this.client = this.loginDetail_string.clientID
+    this.user = this.loginDetail_string.username
+
+    console.log("CLIENT ID", this.client)
+    console.log("USER ID", this.user)
+
+    const test = await this.apiService.GetMaster(this.user + '#user#main', 1);
     this.permission_data = JSON.parse(JSON.parse(JSON.stringify(test.metadata)))
 
-    if(this.permission_data.permission_ID !== 'All'){
+    if (this.permission_data.permission_ID !== 'All') {
 
-    console.log("PERMISSION DATA:",this.permission_data)
+      console.log("PERMISSION DATA:", this.permission_data)
 
-    const permisson_response = await this.apiService.GetMaster(this.client+'#permission#'+this.permission_data.permission_ID+'#main',1);
+      const permisson_response = await this.apiService.GetMaster(this.client + '#permission#' + this.permission_data.permission_ID + '#main', 1);
 
-    this.permission_data = JSON.parse(JSON.parse(JSON.stringify(permisson_response.metadata)))
+      this.permission_data = JSON.parse(JSON.parse(JSON.stringify(permisson_response.metadata)))
 
-    console.log("GET PERMISSION DATA RESPONSE:",this.permission_data.dreamBoardIDs)
+      console.log("GET PERMISSION DATA RESPONSE:", this.permission_data.dreamBoardIDs)
 
-    const userPermissions = JSON.stringify(this.permission_data.dreamBoardIDs|| []);
-    // const userPermissions = ['view-dreamboard/Dreamboard_4'];
+      const userPermissions = JSON.stringify(this.permission_data.dreamBoardIDs || []);
+      // const userPermissions = ['view-dreamboard/Dreamboard_4'];
 
-    console.log("USER DREAMBOARD PERMISSION",userPermissions)
+      console.log("USER DREAMBOARD PERMISSION", userPermissions)
 
-    this.route.paramMap.subscribe(async params => {
-      console.log(this.route);
-      this.id = params.get('id')
-      this.form_id = params.get('formId')
-      
-      // this.form_id = params.get('formId') || 'All'; // Default to 'All' if no formId is found
-      // console.log(this.formId);
-      console.log(this.id);
+      this.route.paramMap.subscribe(async params => {
+        console.log(this.route);
+        this.id = params.get('id')
+        this.form_id = params.get('formId')
 
-
-      this.project =  await this.apiService.GetMaster(this.client+'#project#'+this.form_id+'#main',1)
-
-      if(this.project && this.project.PK){
-
-      console.log("PROJECT:",(this.project))
-
-      this.project.PK = this.project.PK.replace(/#/g, "_");
-
-      this.project.metadata = JSON.parse(this.project.metadata)
-
-      console.log(this.project); 
-      }
+        // this.form_id = params.get('formId') || 'All'; // Default to 'All' if no formId is found
+        // console.log(this.formId);
+        console.log(this.id);
 
 
-  
-      this.route.queryParamMap.subscribe(queryParams => {
-        // Handle query parameters such as formId and recordId
-        this.formId = queryParams.get('formId');
-        this.recordId = queryParams.get('recordId');
-        
-        let params_url = '';
-  
-        if (this.formId && this.formId.length > 0) {
-          params_url = `&formId=${this.formId}`;
+        this.project = await this.apiService.GetMaster(this.client + '#project#' + this.form_id + '#main', 1)
+
+        if (this.project && this.project.PK) {
+
+          console.log("PROJECT:", (this.project))
+
+          this.project.PK = this.project.PK.replace(/#/g, "_");
+
+          this.project.metadata = JSON.parse(this.project.metadata)
+
+          console.log(this.project);
         }
-        // if (this.form_id && this.form_id.length > 0) {
-        //   params_url = `&formId=${this.form_id}`;
-        // }
-
-        console.log("PARAMS URL:",params_url)
 
 
-        if(params_url && params_url.length > 0 && this.recordId && this.recordId.length > 0)
-          params_url = params_url + '&recordId='+this.recordId;
 
-        if(params_url.length>1){
+        this.route.queryParamMap.subscribe(queryParams => {
+          // Handle query parameters such as formId and recordId
+          this.formId = queryParams.get('formId');
+          this.recordId = queryParams.get('recordId');
 
-          if (this.form_id && this.form_id.length > 0) {
-            // params_url = params_url+`&formId=${this.form_id}`;
+          let params_url = '';
 
-            params_url = params_url+`&formId=All`;
+          if (this.formId && this.formId.length > 0) {
+            params_url = `&formId=${this.formId}`;
+          }
+          // if (this.form_id && this.form_id.length > 0) {
+          //   params_url = `&formId=${this.form_id}`;
+          // }
+
+          console.log("PARAMS URL:", params_url)
+
+
+          if (params_url && params_url.length > 0 && this.recordId && this.recordId.length > 0)
+            params_url = params_url + '&recordId=' + this.recordId;
+
+          if (params_url.length > 1) {
+
+            if (this.form_id && this.form_id.length > 0) {
+              // params_url = params_url+`&formId=${this.form_id}`;
+
+              params_url = params_url + `&formId=All`;
+            }
+
+          }
+          else {
+            if (this.form_id && this.form_id.length > 0) {
+              params_url = `&formId=${this.form_id}`;
+            }
           }
 
-        }
-        else{
-           if (this.form_id && this.form_id.length > 0) {
-          params_url = `&formId=${this.form_id}`;
-        }
-        }
+          console.log("AFTER RECORD ID:", params_url)
 
-        console.log("AFTER RECORD ID:",params_url)
-  
-        // API call to fetch Dreamboard data
-        var x = this.apiService.GetMaster('WIMATE_ADMIN' + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
-        // var x = this.apiService.GetMaster(this.client + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
-          this.response = JSON.parse(res.metadata);
-          this.url_result = this.response.HTML;
-          console.log("URL RES", this.url_result);
-          const timestamp = new Date().getTime();
-  
-          // Build the URL based on whether formId is provided or not
-          this.url = `https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/` + this.url_result +
-            `?t=${timestamp}` +
-            `&loginDetail=${JSON.stringify(JSON.stringify(this.loginDetail_string))}` +
-            `&userPermissions=${userPermissions}` +
-            `&theme=${this.theme}` +
-            params_url+`&project=${JSON.stringify((this.project))}`
+          // API call to fetch Dreamboard data
+          var x = this.apiService.GetMaster('WIMATE_ADMIN' + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
+            // var x = this.apiService.GetMaster(this.client + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
+            this.response = JSON.parse(res.metadata);
+            this.url_result = this.response.HTML;
+            console.log("URL RES", this.url_result);
+            const timestamp = new Date().getTime();
 
-           
-  
-          this.send_data = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-          console.log(this.send_data);
-          this.changeDetection.detectChanges();
+            // Build the URL based on whether formId is provided or not
+            this.url = `https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/` + this.url_result +
+              `?t=${timestamp}` +
+              `&loginDetail=${JSON.stringify(JSON.stringify(this.loginDetail_string))}` +
+              `&userPermissions=${userPermissions}` +
+              `&theme=${this.theme}` +
+              params_url + `&project=${JSON.stringify((this.project))}`+
+              `&cacheBuster=${cacheBustingToken}`;
+
+
+
+            this.send_data = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+            this.iframeSrc = this.send_data;
+
+            console.log(this.send_data);
+            this.iframeSrc = ''; // Temporarily clear the iframeSrc
+            this.changeDetection.detectChanges(); // Trigger change detection
+            this.iframeSrc = this.send_data; // Assign the updated URL
+          // this.changeDetection.detectChanges();
+            this.changeDetection.detectChanges();
+          });
         });
+
       });
+    }
+    else {
 
-    });
-   }
-   else{
+      const userPermissions = 'All'
+      this.route.paramMap.subscribe(async params => {
+        console.log(this.route);
+        this.id = params.get('id')
+        this.form_id = params.get('formId')
 
-    const userPermissions = 'All'
-    this.route.paramMap.subscribe(async params => {
-      console.log(this.route);
-      this.id = params.get('id')
-      this.form_id = params.get('formId')
-      
-      // this.form_id = params.get('formId') || 'All'; // Default to 'All' if no formId is found
-      // console.log(this.formId);
-      console.log(this.id);
+        // this.form_id = params.get('formId') || 'All'; // Default to 'All' if no formId is found
+        // console.log(this.formId);
+        console.log(this.id);
 
 
-      this.project =  await this.apiService.GetMaster(this.client+'#project#'+this.form_id+'#main',1)
+        this.project = await this.apiService.GetMaster(this.client + '#project#' + this.form_id + '#main', 1)
 
-      if(this.project && this.project.PK){
+        if (this.project && this.project.PK) {
 
-      console.log("PROJECT:",(this.project))
+          console.log("PROJECT:", (this.project))
 
-      this.project.PK = this.project.PK.replace(/#/g, "_");
+          this.project.PK = this.project.PK.replace(/#/g, "_");
 
-      this.project.metadata = JSON.parse(this.project.metadata)
+          this.project.metadata = JSON.parse(this.project.metadata)
 
-      console.log(this.project); 
-      }
-
-
-  
-      this.route.queryParamMap.subscribe(queryParams => {
-        // Handle query parameters such as formId and recordId
-        this.formId = queryParams.get('formId');
-        this.recordId = queryParams.get('recordId');
-        
-        let params_url = '';
-  
-        if (this.formId && this.formId.length > 0) {
-          params_url = `&formId=${this.formId}`;
+          console.log(this.project);
         }
-        // if (this.form_id && this.form_id.length > 0) {
-        //   params_url = `&formId=${this.form_id}`;
-        // }
-
-        console.log("PARAMS URL:",params_url)
 
 
-        if(params_url && params_url.length > 0 && this.recordId && this.recordId.length > 0)
-          params_url = params_url + '&recordId='+this.recordId;
 
-        if(params_url.length>1){
+        this.route.queryParamMap.subscribe(queryParams => {
+          // Handle query parameters such as formId and recordId
+          this.formId = queryParams.get('formId');
+          this.recordId = queryParams.get('recordId');
 
-          if (this.form_id && this.form_id.length > 0) {
-            // params_url = params_url+`&formId=${this.form_id}`;
+          let params_url = '';
 
-            params_url = params_url+`&formId=All`;
+          if (this.formId && this.formId.length > 0) {
+            params_url = `&formId=${this.formId}`;
+          }
+          // if (this.form_id && this.form_id.length > 0) {
+          //   params_url = `&formId=${this.form_id}`;
+          // }
+
+          console.log("PARAMS URL:", params_url)
+
+
+          if (params_url && params_url.length > 0 && this.recordId && this.recordId.length > 0)
+            params_url = params_url + '&recordId=' + this.recordId;
+
+          if (params_url.length > 1) {
+
+            if (this.form_id && this.form_id.length > 0) {
+              // params_url = params_url+`&formId=${this.form_id}`;
+
+              params_url = params_url + `&formId=All`;
+            }
+
+          }
+          else {
+            if (this.form_id && this.form_id.length > 0) {
+              params_url = `&formId=${this.form_id}`;
+            }
           }
 
-        }
-        else{
-           if (this.form_id && this.form_id.length > 0) {
-          params_url = `&formId=${this.form_id}`;
-        }
-        }
+          console.log("AFTER RECORD ID:", params_url)
 
-        console.log("AFTER RECORD ID:",params_url)
-  
-        // API call to fetch Dreamboard data
-        var x = this.apiService.GetMaster('WIMATE_ADMIN' + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
-        // var x = this.apiService.GetMaster(this.client + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
-          this.response = JSON.parse(res.metadata);
-          this.url_result = this.response.HTML;
-          console.log("URL RES", this.url_result);
-          const timestamp = new Date().getTime();
-  
-          // Build the URL based on whether formId is provided or not
-          this.url = `https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/` + this.url_result +
-            `?t=${timestamp}` +
-            `&loginDetail=${JSON.stringify(JSON.stringify(this.loginDetail_string))}` +
-            `&userPermissions=${userPermissions}` +
-            `&theme=${this.theme}` +
-            params_url+`&project=${JSON.stringify((this.project))}`
+          // API call to fetch Dreamboard data
+          var x = this.apiService.GetMaster('WIMATE_ADMIN' + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
+            // var x = this.apiService.GetMaster(this.client + "#dreamboard#" + this.id + "#main", 1).then((res: any) => {
+            this.response = JSON.parse(res.metadata);
+            this.url_result = this.response.HTML;
+            console.log("URL RES", this.url_result);
+            const timestamp = new Date().getTime();
 
-           
-  
-          this.send_data = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-          console.log(this.send_data);
-          this.changeDetection.detectChanges();
+            // Build the URL based on whether formId is provided or not
+            this.url = `https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/` + this.url_result +
+              `?t=${timestamp}` +
+              `&loginDetail=${JSON.stringify(JSON.stringify(this.loginDetail_string))}` +
+              `&userPermissions=${userPermissions}` +
+              `&theme=${this.theme}` +
+              params_url + `&project=${JSON.stringify((this.project))}`+
+              `&cacheBuster=${cacheBustingToken}`;
+
+
+
+            this.send_data = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+            this.iframeSrc = this.send_data;
+            console.log(this.send_data);
+            this.iframeSrc = ''; // Temporarily clear the iframeSrc
+            this.changeDetection.detectChanges(); // Trigger change detection
+            this.iframeSrc = this.send_data;
+            this.changeDetection.detectChanges();
+          });
         });
-      });
 
-    });
-   }
-    
+      });
+    }
+
   }
 }
 
