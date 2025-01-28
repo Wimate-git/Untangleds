894c8ebaeb8dd5683bd391bb38f3d5c80807c811
Swapnil P <swapnil@Swapnils-MacBook-Air.local>
2025-01-28 14:47:14 +0530

    swapnil

--- src/app/pages/dashbordForm/dashboardForm.component.ts
+++ src/app/pages/dashbordForm/dashboardForm.component.ts
@@ -2,7 +2,7 @@ import { Component, HostBinding, Input, OnInit, ViewChild } from '@angular/core'
 import { ModalConfig, ModalComponent } from '../../_metronic/partials';
 import { APIService } from 'src/app/API.service';
 import { ChangeDetectorRef } from '@angular/core';
-import Swal,{ SweetAlertOptions } from 'sweetalert2';
+import Swal, { SweetAlertOptions } from 'sweetalert2';
 import { ActivatedRoute, Route } from "@angular/router";
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
@@ -15,10 +15,10 @@ import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 export class DashboardFormComponent implements OnInit {
 
 
-  swalOptions: SweetAlertOptions = {};
-  @ViewChild('noticeSwal')
+    swalOptions: SweetAlertOptions = {};
+    @ViewChild('noticeSwal')
 
-  noticeSwal: SwalComponent;
+    noticeSwal: SwalComponent;
 
     login_detail: any;
     loginDetail_string: any;
@@ -37,17 +37,20 @@ export class DashboardFormComponent implements OnInit {
     groupFormResponse: any;
     groupForm: any;
     group: any[] = [];
-    resultArray:any[] =[];
+    resultArray: any[] = [];
     loading = true;
     id: string | null;
-  
+    formIDPermission: any;
+    locationPermission: any;
+    filtermatchedData: any;
+
 
 
     constructor(
         private api: APIService,
         private cdr: ChangeDetectorRef,
         private route: ActivatedRoute,
-        private spinner:NgxSpinnerService
+        private spinner: NgxSpinnerService
     ) { }
 
 
@@ -70,203 +73,241 @@ export class DashboardFormComponent implements OnInit {
 
         this.client = this.loginDetail_string.clientID
         this.user = this.loginDetail_string.username
+    
+
+        console.log("FORM ID PERMISSION:",this.formIDPermission)
+        console.log("LOCATION PERMISSION:",this.locationPermission )
 
         const test = await this.api.GetMaster(this.user + '#user#main', 1);
         this.permission_data = JSON.parse(JSON.parse(JSON.stringify(test.metadata)))
 
         console.log("PERMISSION DATA:", this.permission_data)
+        this.formIDPermission = this.permission_data.form_permission
+        this.locationPermission = this.permission_data.location_permission
+        console.log("FORM ID PERMISSION:",this.formIDPermission)
+        console.log("LOCATION PERMISSION:",this.locationPermission )
 
-        if(this.permission_data.permission_ID !== 'All'){
+        if (this.permission_data.permission_ID !== 'All') {
 
-        const permisson_response = await this.api.GetMaster(this.client + '#permission#' + this.permission_data.permission_ID + '#main', 1);
+            const permisson_response = await this.api.GetMaster(this.client + '#permission#' + this.permission_data.permission_ID + '#main', 1);
 
-        this.permission_data = JSON.parse(JSON.parse(JSON.stringify(permisson_response.metadata)))
+            this.permission_data = JSON.parse(JSON.parse(JSON.stringify(permisson_response.metadata)))
 
-        console.log("GET PERMISSION DATA RESPONSE:", JSON.parse(this.permission_data.dynamicEntries))
+            console.log("GET PERMISSION DATA RESPONSE:", JSON.parse(this.permission_data.dynamicEntries))
 
-        this.permissionForm = JSON.parse(this.permission_data.dynamicEntries)   // Dymaic Entries 
+            this.permissionForm = JSON.parse(this.permission_data.dynamicEntries)   // Dymaic Entries 
 
-        console.log("PERMISSION FORM:",this.permissionForm)
+            console.log("PERMISSION FORM:", this.permissionForm)
 
-        await this.api.GetMaster(this.client + "#formgroup#" + this.form_group + '#main', 1).then((result: any) => {
-
-            this.groupFormResponse = JSON.parse(result.metadata)
-
-
-            console.log("FORMGROUP MAIN:", this.groupFormResponse.formList)
-
-            this.groupForm = this.groupFormResponse.formList
-
-            console.log("GROUP LIST:",this.groupForm)
-
-
-        }).catch((error) => {
-            console.log("FormGroup Error:", error)
-        })
-
-        const allowedForms = this.permissionForm.reduce((acc: string[], permission: any) => {
-            return acc.concat(permission.dynamicForm);
-        }, []);
-
-        console.log("ALLOW FORMS:", allowedForms)
-
-        if(this.id == 'Calendar'){
-            await this.api.GetMaster(this.client + "#systemCalendarQuery#lookup", 1).then((result: any) => {
-                if (result) {
-                    this.helpherObj = JSON.parse(result.options)
-    
-                    this.formList = this.helpherObj.map((item: any) => item)
-    
-                    console.log("DYNAMIC FORMLIST:", this.formList)
-                }
-            }).catch((error) => {
-                console.log("Error:", error)
-            })
-        }
-        else{
-
-
-        await this.api.GetMaster(this.client + "#dynamic_form#lookup", 1).then((result: any) => {
-            if (result) {
-                this.helpherObj = JSON.parse(result.options)
-
-                this.formList = this.helpherObj.map((item: any) => item)
-
-                console.log("DYNAMIC FORMLIST:", this.formList)
-            }
-        }).catch((error) => {
-            console.log("Error:", error)
-        })
-    }
-    
-
-        // await Promise.all([
-        //     this.api.GetMaster(this.client + "#dynamic_form#lookup", 1),
-        //     this.api.GetMaster(this.client + "#systemCalendarQuery#lookup", 1)
-        // ])
-        //     .then((results: any[]) => {
-        //         const [dynamicFormResult, systemCalendarResult] = results;
-        
-        //         // Parse the first result and add to the formList
-        //         if (dynamicFormResult) {
-        //             const dynamicFormOptions = JSON.parse(dynamicFormResult.options);
-        //             this.formList = dynamicFormOptions.map((item: any) => item);
-        //         }
-        
-        //         // Parse the second result and combine with the formList
-        //         if (systemCalendarResult) {
-        //             const systemCalendarOptions = JSON.parse(systemCalendarResult.options);
-        //             const calendarFormList = systemCalendarOptions.map((item: any) => item);
-        
-        //             // Combine the lists
-        //             this.formList = [...this.formList, ...calendarFormList];
-        //         }
-        
-        //         console.log("COMBINED FORMLIST:", this.formList);
-        //     })
-        //     .catch((error) => {
-        //         console.log("Error:", error);
-        //     });
-        
-
-        let matchingItems = []
-        if(allowedForms.includes('All') ){
-            matchingItems =this.groupForm.filter((item: any) => this.groupFormResponse.formList.includes(item));
-        }
-        else{
-         matchingItems = allowedForms.filter((item: any) => this.groupFormResponse.formList.includes(item));
-        }
-
-       
-        console.log("Match Item:", matchingItems)
-
-        // If you want to push the matching items to another array
-        this.resultArray =[]
-        this.resultArray.push(...matchingItems);
-
-        console.log(this.resultArray);
-
-
-        this.cards = this.formList
-            .filter(data => {
-                // Check if the value at index 0 of the formList item matches any value in groupForm
-                return this.resultArray.includes(data[0]);
-            })
-            .map(data => ({
-                // Your card structure here
-                icon: 'https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/Climaveneta/Live+Contract/liveContract.png',
-                name: data[0],  // Name from formList index 0
-                job: data[1],   // Job description from formList index 1
-                avgEarnings: data[3] ? new Date(data[3] * 1000).toLocaleDateString('en-GB', {
-                    day: '2-digit',
-                    month: 'short',
-                    year: 'numeric'
-                }) : '',  // Handle cases where values may be empty
-                totalEarnings: data[0] || '',  // Fallback if index 5 is empty
-                online: this.id  // Placeholder for online status
-            }));
-
-        }
-        else{
             await this.api.GetMaster(this.client + "#formgroup#" + this.form_group + '#main', 1).then((result: any) => {
 
                 this.groupFormResponse = JSON.parse(result.metadata)
-    
-    
+
+
                 console.log("FORMGROUP MAIN:", this.groupFormResponse.formList)
-    
+
                 this.groupForm = this.groupFormResponse.formList
 
-                this.resultArray = this.groupFormResponse.formList
-    
-    
+                if(this.locationPermission.includes('All')){
+
+                    if(!this.formIDPermission.includes('All')){
+
+                        console.log(this.formIDPermission)
+
+                       this.filtermatchedData = this.formIDPermission.filter((item: any) => this.groupForm.includes(item));
+
+                    }
+                    else{
+                        this.filtermatchedData = this.groupForm 
+                    }
+                }
+
+                console.log("GROUP LIST:",this.filtermatchedData)
+
+
             }).catch((error) => {
                 console.log("FormGroup Error:", error)
             })
 
-            await this.api.GetMaster(this.client + "#dynamic_form#lookup", 1).then((result: any) => {
-                if (result) {
-                    this.helpherObj = JSON.parse(result.options)
-    
-                    this.formList = this.helpherObj.map((item: any) => item)
-    
-                    console.log("DYNAMIC FORMLIST:", this.formList)
-                }
-            }).catch((error) => {
-                console.log("Error:", error)
-            })
+            // permission dynamic Entries
+            const allowedForms = this.permissionForm.reduce((acc: string[], permission: any) => {
+                return acc.concat(permission.dynamicForm);
+            }, []);
+
+            console.log("ALLOW FORMS:", allowedForms)
+
+            if (this.id == 'Calendar') {
+                await this.api.GetMaster(this.client + "#systemCalendarQuery#lookup", 1).then((result: any) => {
+                    if (result) {
+                        this.helpherObj = JSON.parse(result.options)
+
+                        this.formList = this.helpherObj.map((item: any) => item)
+
+                        console.log("DYNAMIC FORMLIST:", this.formList)
+                    }
+                }).catch((error) => {
+                    console.log("Error:", error)
+                })
+            }
+            else {
+
+
+                await this.api.GetMaster(this.client + "#dynamic_form#lookup", 1).then((result: any) => {
+                    if (result) {
+                        this.helpherObj = JSON.parse(result.options)
+
+                        this.formList = this.helpherObj.map((item: any) => item)
+
+                        console.log("DYNAMIC FORMLIST:", this.formList)
+                    }
+                }).catch((error) => {
+                    console.log("Error:", error)
+                })
+            }
+
+            let matchingItems = []
+            if (allowedForms.includes('All')) {      //dynamicEntrie selected all 
+                matchingItems = this.filtermatchedData.filter((item: any) => this.groupFormResponse.formList.includes(item));
+            }
+            else {
+                // matchingItems = allowedForms.filter((item: any) => this.groupFormResponse.formList.includes(item));
+
+                matchingItems = this.filtermatchedData.filter((item: any) => this.groupFormResponse.formList.includes(item));
+            }
+
+
+            console.log("Match Item:", matchingItems)
+
+            // If you want to push the matching items to another array
+            this.resultArray = []
+            this.resultArray.push(...matchingItems);
+
+            console.log(this.resultArray);
+
 
             this.cards = this.formList
-            .filter(data => {
-                // Check if the value at index 0 of the formList item matches any value in groupForm
-                return this.resultArray.includes(data[0]);
+                .filter(data => {
+                    // Check if the value at index 0 of the formList item matches any value in groupForm
+                    return this.resultArray.includes(data[0]);
+                })
+                .map(data => ({
+                    // Your card structure here
+                    icon: 'https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/Climaveneta/Live+Contract/liveContract.png',
+                    name: data[0],  // Name from formList index 0
+                    job: data[1],   // Job description from formList index 1
+                    avgEarnings: data[3] ? new Date(data[3] * 1000).toLocaleDateString('en-GB', {
+                        day: '2-digit',
+                        month: 'short',
+                        year: 'numeric'
+                    }) : '',  // Handle cases where values may be empty
+                    totalEarnings: data[0] || '',  // Fallback if index 5 is empty
+                    online: this.id  // Placeholder for online status
+                }));
+
+        }
+        else {
+            await this.api.GetMaster(this.client + "#formgroup#" + this.form_group + '#main', 1).then((result: any) => {
+
+                this.groupFormResponse = JSON.parse(result.metadata)
+
+
+                console.log("FORMGROUP MAIN:", this.groupFormResponse.formList)
+
+                this.groupForm = this.groupFormResponse.formList
+
+                this.resultArray = this.groupFormResponse.formList
+
+                if(this.locationPermission.includes('All')){
+
+                    if(!this.formIDPermission.includes('All')){
+
+                        console.log("FORMId PERMISSION")
+
+                        this.resultArray = this.formIDPermission
+
+                    }
+                }
+                else{
+                    this.resultArray = this.groupFormResponse.formList
+                }
+
+
+            }).catch((error) => {
+                console.log("FormGroup Error:", error)
             })
-            .map(data => ({
-                // Your card structure here
-                icon: 'https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/Climaveneta/Live+Contract/liveContract.png',
-                name: data[0],  // Name from formList index 0
-                job: data[1],   // Job description from formList index 1
-                avgEarnings: data[3] ? new Date(data[3] * 1000).toLocaleDateString('en-GB', {
-                    day: '2-digit',
-                    month: 'short',
-                    year: 'numeric'
-                }) : '',  // Handle cases where values may be empty
-                totalEarnings: data[0] || '',  // Fallback if index 5 is empty
-                online: ''  // Placeholder for online status
-            }));
+
+            // await this.api.GetMaster(this.client + "#dynamic_form#lookup", 1).then((result: any) => {
+            //     if (result) {
+            //         this.helpherObj = JSON.parse(result.options)
+
+            //         this.formList = this.helpherObj.map((item: any) => item)
+
+            //         console.log("DYNAMIC FORMLIST:", this.formList)
+            //     }
+            // }).catch((error) => {
+            //     console.log("Error:", error)
+            // })
+
+            if (this.id == 'Calendar') {
+                await this.api.GetMaster(this.client + "#systemCalendarQuery#lookup", 1).then((result: any) => {
+                    if (result) {
+                        this.helpherObj = JSON.parse(result.options)
+
+                        this.formList = this.helpherObj.map((item: any) => item)
+
+                        console.log("DYNAMIC FORMLIST:", this.formList)
+                    }
+                }).catch((error) => {
+                    console.log("Error:", error)
+                })
+            }
+            else {
+
+
+                await this.api.GetMaster(this.client + "#dynamic_form#lookup", 1).then((result: any) => {
+                    if (result) {
+                        this.helpherObj = JSON.parse(result.options)
+
+                        this.formList = this.helpherObj.map((item: any) => item)
+
+                        console.log("DYNAMIC FORMLIST:", this.formList)
+                    }
+                }).catch((error) => {
+                    console.log("Error:", error)
+                })
+            }
+
+            this.cards = this.formList
+                .filter(data => {
+                    // Check if the value at index 0 of the formList item matches any value in groupForm
+                    return this.resultArray.includes(data[0]);
+                })
+                .map(data => ({
+                    // Your card structure here
+                    icon: 'https://dreamboard-dynamic.s3.ap-south-1.amazonaws.com/public/Climaveneta/Live+Contract/liveContract.png',
+                    name: data[0],  // Name from formList index 0
+                    job: data[1],   // Job description from formList index 1
+                    avgEarnings: data[3] ? new Date(data[3] * 1000).toLocaleDateString('en-GB', {
+                        day: '2-digit',
+                        month: 'short',
+                        year: 'numeric'
+                    }) : '',  // Handle cases where values may be empty
+                    totalEarnings: data[0] || '',  // Fallback if index 5 is empty
+                    online: ''  // Placeholder for online status
+                }));
         }
 
 
         console.log("CARDS ON FORMLIST:", this.cards.length)
 
-        if(this.cards.length == 0){
+        if (this.cards.length == 0) {
             const errorAlert_icon: SweetAlertOptions = {
                 icon: 'error',
                 title: 'No Forms data available.',
                 text: '',
-              };
-      
-              this.showAlert(errorAlert_icon)
+            };
+
+            this.showAlert(errorAlert_icon)
         }
         this.loading = false;
         this.spinner.hide()
@@ -274,29 +315,29 @@ export class DashboardFormComponent implements OnInit {
     }
 
     searchQuery = '';
-  get filteredCards() {
-    return this.cards.filter(card =>
-      card.name?.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      card.job?.toLowerCase().includes(this.searchQuery.toLowerCase())
-    );
-  }
+    get filteredCards() {
+        return this.cards.filter(card =>
+            card.name?.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+            card.job?.toLowerCase().includes(this.searchQuery.toLowerCase())
+        );
+    }
 
 
     showAlert(swalOptions: SweetAlertOptions) {
         let style = swalOptions.icon?.toString() || 'success';
         if (swalOptions.icon === 'error') {
-          style = 'danger';
+            style = 'danger';
         }
         this.swalOptions = Object.assign({
-          buttonsStyling: false,
-          confirmButtonText: "Ok, got it!",
-          customClass: {
-            confirmButton: "btn btn-" + style
-          }
+            buttonsStyling: false,
+            confirmButtonText: "Ok, got it!",
+            customClass: {
+                confirmButton: "btn btn-" + style
+            }
         }, swalOptions);
         this.cdr.detectChanges();
         this.noticeSwal.fire();
-      }
+    }
 
 
 }
